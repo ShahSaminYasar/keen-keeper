1cb430a66da76b61f03c0edb800b517e2a7bafea
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2026-04-13 23:18:48 +0600

    Added: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,48 @@
+import { ChartLine, Clock3, HomeIcon } from "lucide-react";
+import { NavLink } from "react-router";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full bg-white px-3 py-2 h-20 flex items-center">
+      <div className="w-full max-w-7xl mx-auto flex items-center gap-3 justify-between">
+        <img src="/assets/logo.png" alt="Keen Keeper" className="w-50" />
+
+        <nav>
+          <ul className="flex items-center gap-1">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `flex items-center gap-1.5 btn ${isActive ? "btn-primary" : "btn-ghost"}`
+                }
+              >
+                <HomeIcon size={17} /> Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/timeline"
+                className={({ isActive }) =>
+                  `flex items-center gap-1.5 btn ${isActive ? "btn-primary" : "btn-ghost"}`
+                }
+              >
+                <Clock3 size={17} /> Timeline
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/stats"
+                className={({ isActive }) =>
+                  `flex items-center gap-1.5 btn ${isActive ? "btn-primary" : "btn-ghost"}`
+                }
+              >
+                <ChartLine size={17} /> Stats
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 export default Header;
